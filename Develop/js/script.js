cc72b8a7f8365b9e8c3dbc3a1d904520fdf802d4
Camilo Arango <camilo94@icloud.com>
2022-04-22 16:46:37 -0700

    finished function for checking time and formatting the text areas appropriately

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,21 +1,37 @@
 //Date and Time
 var update = function() {
     $("#currentDay").text(moment().format("ddd, MMMM Do YYYY, h:mm:ss a"))
-    pastPresentFuture();
 }
 setInterval(update, 1000);
 
 //check if past present or future
-var whatTime = moment().hour();
-function pastPresentFuture () {
-    if (moment('whatTime').isBefore() == 'true') {
-        
+$("#placeholder > .row > .timelabel").each(function() {
+    var currentTime = moment().hour();
+    if (currentTime > (this.id)){
+        //assign new class (append)
+        console.log(currentTime);
+        $(this).siblings("textarea").addClass("past");
+    } else if (currentTime < (this.id)){
+        //assign new class (append)
+        console.log(currentTime);
+        $(this).siblings("textarea").addClass("future");
+    } else {
+        //assign new class (append)
+        console.log(currentTime);
+        $(this).siblings("textarea").addClass("present");
     }
-}
+});
 
-//save (for loop maybe)
-addEventListener("click", )
-
-//save to local storage
+//save button for saving to local storage
+var toDoList = [];
+document.addEventListener("click", function() {
+    localStorage.setItem("toDoList");
+});
+// $(document).ready(function(){
+//     $(".toDo").on("click", function(){
+//         localStorage.setItem("myContent");
+//         console.log(localStorage.getItem("myContent"));
+//     });
+// });
 
 //load items from local storage
